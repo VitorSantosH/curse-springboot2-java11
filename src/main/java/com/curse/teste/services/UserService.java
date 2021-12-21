63b8cae55f56c7d960e156867ec7a1515c76d810
VitorSantosH <wytorh@gmail.com>
2021-12-21 20:14:12 -0300

    Service layer, component registration

---
+++ src/main/java/com/curse/teste/services/UserService.java
@@ -0,0 +1,30 @@
+package com.curse.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curse.teste.entities.User;
+import com.curse.teste.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+		@Autowired
+		private UserRepository repository;
+		
+		
+		public List<User> findAll(){
+			return repository.findAll();
+		}
+		
+		public User findUser(Long id) {
+			
+			Optional<User> obj = repository.findById(id);
+			
+			return obj.get();
+		}
+	
+}
